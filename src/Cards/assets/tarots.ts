dca850902ae9cbd1f85839e69f829eb72f2e3ef2
lapanikeugene <eugeny.lp@gmail.com>
2022-12-20 22:55:40 +0100

    Add info about tarots

---
+++ src/Cards/assets/tarots.ts
@@ -0,0 +1,190 @@
+export const tarotsDB:{[key:string]: any} = {
+    "Fool":{
+        desc:"New beginnings, innocence, adventure. ",
+        answer:"Yes",
+        image:"0fool.jpg"
+    },
+    "The Magician":{
+        desc:"Willpower, creation, manifestation",
+        answer:"Yes",
+        image:"1magician.jpg"
+    },
+    "The High Priestess":{
+        desc:"Intuitive, unconscious, divine feminine",
+        answer:"Unknown",
+        image:"2priestess.jpg"
+    },
+    "The Empress":{
+        desc:"Femininity, nurturing, fertility, abundance",
+        answer:"Yes",
+        image:"3empress.jpg"
+    },
+    "The Emperor":{
+        desc:"Authority, structure, a father figure",
+        answer:"Yes",
+        image:"4emperor.jpg"
+    },
+    "The Hierophant":{
+        desc:"Spiritual wisdom, tradition, conformity, morality, ethics",
+        answer:"Neutral",
+        image:"5hierophant.jpg"
+    },
+    "The Lovers":{
+        desc:"Love, harmony, partnerships, choices",
+        answer:"Maybe",
+        image:"6lovers.jpg"
+    },
+    "The Chariot":{
+        desc:"Direction, control, willpower, determination, success, action",
+        answer:"Yes",
+        image:"7chariot.jpg"
+    },
+    "Strength":{
+        desc:"Strength, courage, compassion, focus, persuasion, influence",
+        answer:"Yes",
+        image:"8strength.jpg"
+    },
+    "The Hermit":{
+        desc:"Wisdom, soul searching, solitude, spiritual enlightenment, receiving or giving guidance",
+        answer:"No",
+        image:"9hermit.jpg"
+    },
+
+    "Wheel of Fortune":{
+        desc:"Chance, destiny and fate, karma, turning points",
+        answer:"Yes",
+        image:"10fortune.jpg"
+    },
+
+    "Justice":{
+        desc:"Fairness, integrity, legal disputes, cause and effect, life lessons",
+        answer:"No",
+        image:"10justice.jpg"
+    },
+    "The Hanged Man":{
+        desc:"Letting go, sacrificing, pausing to reflect, uncertainty, spiritual development",
+        answer:"Not sure",
+        image:"11hanged.jpg"
+    },
+    "Death":{
+        desc:"Ending of a cycle, transitions, getting rid of excess, powerful movement, resolutions",
+        answer:"Yes, but in different way",
+        image:"13death.jpg"
+    },
+    "Temperance":{
+        desc:"Balance, moderation, good health, cooperating with others, finding solutions",
+        answer:"Yes, but need to wait",
+        image:"14Temperance.jpg"
+    },
+    "Devil":{
+        desc:"Material focus, trapped in bondage, addictions and depression, negative thinking, betrayal",
+        answer:"No",
+        image:"15Devil.jpg"
+    },
+    "The Tower":{
+        desc:"Intense and sudden change, release, painful loss, tragedy, revelation",
+        answer:"No",
+        image:"16Tower.jpg"
+    },
+    "The Star":{
+        desc:"Hope, renewal, creativity and inspiration, generosity, healing",
+        answer:"Yes, but you need to wait so long...",
+        image:"17Star.jpg"
+    },
+    "The Moon":{
+        desc:"Fear, anxiety, confusion, delusion, risk",
+        answer:"No",
+        image:"18Moon.jpg"
+    },
+    "The Sun":{
+        desc:"Happiness, fertility, success, optimism, truth",
+        answer:"Yes",
+        image:"19Sun.jpg"
+    },
+        
+    "Judgement":{
+            desc:"Reflection, inner calling, reckoning, awakening, rebirth, absolution",
+            answer:"You will get, that you are deserve",
+            image:"20Judgement.jpg"
+        },
+    "The World":{
+            desc:"Reflection, inner calling, reckoning, awakening, rebirth, absolution",
+            answer:"No, situation will end up",
+            image:"21World.jpg"
+        },
+
+
+
+       
+        "Ace of Cups":{
+            desc:"Reflection, inner calling, reckoning, awakening, rebirth, absolution",
+            answer:"Unknown, ask later",
+            image:"21World.jpg"
+        },
+        "Two of Cups":{
+            desc:"Reflection, inner calling, reckoning, awakening, rebirth, absolution",
+            answer:"Unknown, ask later",
+            image:"21World.jpg"
+        },
+        "Three of Cups":{
+            desc:"Reflection, inner calling, reckoning, awakening, rebirth, absolution",
+            answer:"Unknown, ask later",
+            image:"21World.jpg"
+        },
+        "Four of Cups":{
+            desc:"Reflection, inner calling, reckoning, awakening, rebirth, absolution",
+            answer:"Unknown, ask later",
+            image:"21World.jpg"
+        },
+        "Five of Cups":{
+            desc:"Reflection, inner calling, reckoning, awakening, rebirth, absolution",
+            answer:"Unknown, ask later",
+            image:"21World.jpg"
+        },
+        "Six of Cups":{
+            desc:"Reflection, inner calling, reckoning, awakening, rebirth, absolution",
+            answer:"Unknown, ask later",
+            image:"21World.jpg"
+        },
+        "Seven of Cups":{
+            desc:"Reflection, inner calling, reckoning, awakening, rebirth, absolution",
+            answer:"Unknown, ask later",
+            image:"21World.jpg"
+        },
+        "Eight of Cups":{
+            desc:"Reflection, inner calling, reckoning, awakening, rebirth, absolution",
+            answer:"Unknown, ask later",
+            image:"21World.jpg"
+        },
+        "Nine of Cups":{
+            desc:"Reflection, inner calling, reckoning, awakening, rebirth, absolution",
+            answer:"Unknown, ask later",
+            image:"21World.jpg"
+        },
+        "Ten of Cups":{
+            desc:"Reflection, inner calling, reckoning, awakening, rebirth, absolution",
+            answer:"Unknown, ask later",
+            image:"21World.jpg"
+        },
+        "Page of Cups":{
+            desc:"Reflection, inner calling, reckoning, awakening, rebirth, absolution",
+            answer:"Unknown, ask later",
+            image:"21World.jpg"
+        },
+        "Knight of Cups":{
+            desc:"Reflection, inner calling, reckoning, awakening, rebirth, absolution",
+            answer:"Unknown, ask later",
+            image:"21World.jpg"
+        },
+        "Queen of Cups":{
+            desc:"Reflection, inner calling, reckoning, awakening, rebirth, absolution",
+            answer:"Unknown, ask later",
+            image:"21World.jpg"
+        },
+        "King of Cups":{
+            desc:"Reflection, inner calling, reckoning, awakening, rebirth, absolution",
+            answer:"Unknown, ask later",
+            image:"21World.jpg"
+        },
+        
+    }
